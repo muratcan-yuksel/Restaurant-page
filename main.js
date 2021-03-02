0c7f6d06512b2661fd8b651e3a599c404ca951db
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-03-02 08:12:11 +0300

    added the id header to the image via DOM

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";!function(){console.log("fooo");const e=document.createElement("IMG");e.setAttribute("src","/TOP/Restaurant-page/pics/header.png"),document.body.appendChild(e)}(),foo})();
+(()=>{"use strict";!function(){document.getElementById("content"),document.createElement("P").textContent="hoofs";const e=document.createElement("IMG");e.setAttribute("src","/TOP/Restaurant-page/pics/header.png"),e.setAttribute("id","header"),document.body.appendChild(e)}(),foo})();
