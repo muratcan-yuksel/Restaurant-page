3fbb8d91d0e7ccab93a98f5e0c1468c1f3f931be
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-03-03 01:40:18 +0300

    added the name and id for the first label

--- src/layout.js
+++ src/layout.js
@@ -43,42 +43,40 @@ const labels = (() => {
         //create the radio element
         const radio1= document.createElement("INPUT");
         radio1.setAttribute("type", "radio");
+        radio1.setAttribute("id", "tab2-1");
+        radio1.setAttribute("name", "tabs-two");
         radio1.checked="checked";
         //set the attribute and append it
         label1.setAttribute("for", radio1);
         label1.appendChild(radio1);
-
+        
         return radio1,label1;
     } 
 
- 
-  
+    const labelRadioTwo=()=>{
 
-    
+
+    }
+  
 return {labelRadioOne}
 })();
 
-{/* <div>
-<h4>Tab One</h4>
-<p>Une cuisine qui nous fait voyager du moyen-orient au plus large bassin méditerranéen.Tout cela, dans une ambiance conviale, situé dans un quartier vivant et multi-culturel. De la street food, de qualité, sur place et à emporter.</p>
-<p>On y mange quoi? Des mezzes (houmous au tahin maison, des boreks à la feta aop, des falafels aux fèves), des salades, des pitas, bref du bon et du faitmaison. Une cuisine sensée qui laisse la part belle aux légumineuses et aux légumes frais, dans le respect de la saisonnalité et des saveurs. Des ingrédients secrets qui revisitent les grand classiques de la cuisine méditerranéenne.</p>
-<p>Le plus important pour nous? Rendre accessible une cuisinesaine, gourmande et de qualité. Tout le monde a le droit de manger du BON . Nous travaillons avec des fournisseurs et des producteurs exigeants qui nous offrent la possibilité de sublimer des ingrédients de première qualité.</p>
-</div> */}
 const firstPageContent = (()=>{
     //these will be appended to the first tab-2
     const paragraphs = () =>{
+        //add paragraphs
         const para1= document.createElement("P");
             para1.textContent= "Une cuisine qui nous fait voyager du moyen-orient au plus large bassin méditerranéen.Tout cela, dans une ambiance conviale, situé dans un quartier vivant et multi-culturel. De la street food, de qualité, sur place et à emporter."
         const para2= document.createElement("P");
             para2.textContent="On y mange quoi? Des mezzes (houmous au tahin maison, des boreks à la feta aop, des falafels aux fèves), des salades, des pitas, bref du bon et du faitmaison. Une cuisine sensée qui laisse la part belle aux légumineuses et aux légumes frais, dans le respect de la saisonnalité et des saveurs. Des ingrédients secrets qui revisitent les grand classiques de la cuisine méditerranéenne."
         const para3= document.createElement("P");
             para3.textContent="Le plus important pour nous? Rendre accessible une cuisinesaine, gourmande et de qualité. Tout le monde a le droit de manger du BON . Nous travaillons avec des fournisseurs et des producteurs exigeants qui nous offrent la possibilité de sublimer des ingrédients de première qualité. "
-    
+        // create header and append the paragraphs into it
         const header4= document.createElement("H4");
             header4.appendChild(para1);
             header4.appendChild(para2);
             header4.appendChild(para3);
-
+        //create an empty div and append the header into it
             const div = document.createElement("div");
             div.appendChild(header4);
             return div;
@@ -98,10 +96,9 @@ const divTabs = () => {
     firstTab2.setAttribute("class", "tab-2");
     //append this first tab2 to the tabs
     tabs.appendChild(firstTab2);
-
-    console.log(firstPageContent.paragraphs());
+    //append the radio and the label
     firstTab2.appendChild(labels.labelRadioOne());
-
+    //append the paragraphs
     firstTab2.appendChild(firstPageContent.paragraphs());
    
     //get the 2nd tab-2
