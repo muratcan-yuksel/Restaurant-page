74e7b6606ff006a3bdc628768b3a04dd42df4fcb
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-03-03 01:28:31 +0300

    corrected typos but my code doesn't work

--- src/layout.js
+++ src/layout.js
@@ -60,9 +60,9 @@ return {labelRadioOne}
 
 {/* <div>
 <h4>Tab One</h4>
-<p>Une cuisine qui nous fait #voyager du moyen-orient au plus large bassin #méditerranéen.Tout cela, dans une ambiance #conviale, situé dans un quartier vivant et multi-culturel. De la street food, de qualité, sur place et à emporter.</p>
-<p>On y mange quoi? Des #mezzes (#houmous au #tahin maison, des #boreks à la #feta aop, des #falafels aux fèves), des salades, des #pitas, bref du bon et du #faitmaison. Une #cuisine sensée qui laisse la part belle aux légumineuses et aux #légumes frais, dans le respect de la saisonnalité et des #saveurs. Des ingrédients secrets qui revisitent les grand classiques de la cuisine méditerranéenne.</p>
-<p>Le plus important pour nous? Rendre accessible une #cuisinesaine, gourmande et de qualité. Tout le monde a le droit de manger du #BON . Nous travaillons avec des fournisseurs et des producteurs exigeants qui nous offrent la possibilité de sublimer des ingrédients de première qualité.</p>
+<p>Une cuisine qui nous fait voyager du moyen-orient au plus large bassin méditerranéen.Tout cela, dans une ambiance conviale, situé dans un quartier vivant et multi-culturel. De la street food, de qualité, sur place et à emporter.</p>
+<p>On y mange quoi? Des mezzes (houmous au tahin maison, des boreks à la feta aop, des falafels aux fèves), des salades, des pitas, bref du bon et du faitmaison. Une cuisine sensée qui laisse la part belle aux légumineuses et aux légumes frais, dans le respect de la saisonnalité et des saveurs. Des ingrédients secrets qui revisitent les grand classiques de la cuisine méditerranéenne.</p>
+<p>Le plus important pour nous? Rendre accessible une cuisinesaine, gourmande et de qualité. Tout le monde a le droit de manger du BON . Nous travaillons avec des fournisseurs et des producteurs exigeants qui nous offrent la possibilité de sublimer des ingrédients de première qualité.</p>
 </div> */}
 const firstPageContent = (()=>{
     //these will be appended to the first tab-2
@@ -70,11 +70,21 @@ const firstPageContent = (()=>{
         const para1= document.createElement("P");
             para1.textContent= "Une cuisine qui nous fait voyager du moyen-orient au plus large bassin méditerranéen.Tout cela, dans une ambiance conviale, situé dans un quartier vivant et multi-culturel. De la street food, de qualité, sur place et à emporter."
         const para2= document.createElement("P");
-            para2.textContent="On y mange quoi? Des #mezzes (#houmous au #tahin maison, des #boreks à la #feta aop, des #falafels aux fèves), des salades, des #pitas, bref du bon et du #faitmaison. Une #cuisine sensée qui laisse la part belle aux légumineuses et aux #légumes frais, dans le respect de la saisonnalité et des #saveurs. Des ingrédients secrets qui revisitent les grand classiques de la cuisine méditerranéenne."
-        const para2= document.createElement("P");
-            para3.textContent="Le plus important pour nous? Rendre accessible une #cuisinesaine, gourmande et de qualité. Tout le monde a le droit de manger du #BON . Nous travaillons avec des fournisseurs et des producteurs exigeants qui nous offrent la possibilité de sublimer des ingrédients de première qualité. "
-    }
+            para2.textContent="On y mange quoi? Des mezzes (houmous au tahin maison, des boreks à la feta aop, des falafels aux fèves), des salades, des pitas, bref du bon et du faitmaison. Une cuisine sensée qui laisse la part belle aux légumineuses et aux légumes frais, dans le respect de la saisonnalité et des saveurs. Des ingrédients secrets qui revisitent les grand classiques de la cuisine méditerranéenne."
+        const para3= document.createElement("P");
+            para3.textContent="Le plus important pour nous? Rendre accessible une cuisinesaine, gourmande et de qualité. Tout le monde a le droit de manger du BON . Nous travaillons avec des fournisseurs et des producteurs exigeants qui nous offrent la possibilité de sublimer des ingrédients de première qualité. "
+    
+        const header4= document.createElement("H4");
+            header4.appendChild(para1);
+            header4.appendChild(para2);
+            header4.appendChild(para3);
 
+            const div = document.createElement("div");
+            div.appendChild(header4);
+
+        }
+
+        return {paragraphs}
 })();
 
 
@@ -91,6 +101,8 @@ const divTabs = () => {
 
     
     firstTab2.appendChild(labels.labelRadioOne());
+
+    firstTab2.appendChild(firstPageContent.paragraphs());
    
     //get the 2nd tab-2
     const secondTab2= document.createElement("div");
