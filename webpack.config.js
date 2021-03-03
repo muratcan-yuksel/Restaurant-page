a4f62cdf31f87e44ecff96bfd82801a1223d6bab
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-03-03 11:11:30 +0300

    recovered the config file

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,5 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
   },
-  plugins: [
-    new HtmlWebpackPlugin(), // Generates default index.html
-    new HtmlWebpackPlugin({  // Also generate a test.html
-      filename: 'menu.html',
-      template: 'src/assets/menu.html'
-    })
-  ]
 };
