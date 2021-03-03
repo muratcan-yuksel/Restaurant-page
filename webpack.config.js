a7763c61b0d8558e01969a7458853dd5b31c995d
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-03-03 10:23:00 +0300

    modified webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,13 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin(), // Generates default index.html
+    new HtmlWebpackPlugin({  // Also generate a test.html
+      filename: 'test.html',
+      template: 'src/assets/test.html'
+    })
+  ]
 };
